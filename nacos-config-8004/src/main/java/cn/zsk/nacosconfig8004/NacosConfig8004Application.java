af9c8b624108b9ed93c7a4bc3ea556522870992c
zsk <303316861@qq.com>
2021-07-17 00:10:09 +0800

    feat:规整pom文件 feat:增加nacos配置中心demo

---
+++ nacos-config-8004/src/main/java/cn/zsk/nacosconfig8004/NacosConfig8004Application.java
@@ -0,0 +1,13 @@
+package cn.zsk.nacosconfig8004;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosConfig8004Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfig8004Application.class, args);
+    }
+
+}
